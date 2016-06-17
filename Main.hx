4aeba32ffe637459cad68660bdfb0d31c28b39c0
sam <sam@silver42.karoo.co.uk>
2016-06-17 01:21:02 +0100

    Start writing code to read the RLE, life and cells pattern formats, add code to select the files and display them in an editable textedit etc

--- Main.hx
+++ Main.hx
@@ -2,15 +2,12 @@ package;
 
 import js.Browser;
 import js.html.DivElement;
+import js.html.SelectElement;
 import shaders.Life;
-import three.Color;
-import three.Material;
 import three.Mesh;
-import three.MeshLambertMaterial;
 import three.OrthographicCamera;
 import three.PixelFormat;
 import three.PlaneBufferGeometry;
-import three.PlaneGeometry;
 import three.Scene;
 import three.ShaderMaterial;
 import three.Texture;
@@ -19,6 +16,9 @@ import three.WebGLRenderTarget;
 import three.WebGLRenderTargetOptions;
 import three.WebGLRenderer;
 import webgl.Detector;
+import js.html.TextAreaElement;
+
+using StringTools;
 
 // Automatic HTML code completion, you need to point these to your debug/release HTML
 #if debug
@@ -26,6 +26,13 @@ import webgl.Detector;
 #else
 @:build(CodeCompletion.buildLocalFile("bin/release/index.html"))
 #end
+class ID {}
+
+@:build(PatternFileReaderMacro.build("embed"))
+@:keep
+class Patterns {
+	// Contains whole pattern (gliders etc) files as arrays of strings for use at runtime
+}
 
 class GameOfLife {
 	private var camera:OrthographicCamera;
@@ -76,6 +83,96 @@ class GameOfLife {
 	
 	public function stampPattern(x:Int, y:Int, pattern:Texture):Void {
 		// todo render pattern texture/pixels to rendertarget
+		
+	}
+}
+
+// Expands and converts ASCII Life 1.0x format patterns: http://www.conwaylife.com/wiki/Life_1.05
+class LifeReader {
+	// TODO
+}
+
+// Expands and converts "plain text" cells format patterns: http://www.conwaylife.com/wiki/Plaintext
+class PlaintextCellsReader {
+	// TODO
+}
+
+// Expands and converts run length encoded patterns: http://www.conwaylife.com/wiki/RLE
+class RLEReader {
+	public static function expandRle(rle:Array<String>):Array<String> {
+		var width:Int = 0;
+		var height:Int = 0;
+		var rule:String = "";
+		var rlePattern:String = "";
+		var runCountMatcher:EReg = ~/[0-9]/i;
+		var foundSize:Bool = false;
+		
+		for (line in rle) {
+			if (line.indexOf("#") != -1) {
+				continue; // Ignore the # lines
+			}
+			
+			if (!foundSize) { // Look for the width, height and rule
+				var components:Array<String> = line.split(",");
+				Sure.sure(components.length == 2 || components.length == 3);
+				
+				var getComponentValue = function(component:String):String {
+					var kv = component.split("=");
+					Sure.sure(kv.length == 2);
+					var v = kv[1].trim();
+					Sure.sure(v.length != 0);
+					return v;
+				}
+				
+				width = Std.parseInt(getComponentValue(components[0]));
+				height = Std.parseInt(getComponentValue(components[1]));
+				
+				if(components.length == 3) {
+					rule = getComponentValue(components[2]);
+				}
+				
+				foundSize = true;
+				continue;
+			}
+			
+			rlePattern += line; // Flatten all other lines of RLE encoded b (dead) o (alive) and $ (end of line) cell descriptions into a single string
+		}
+		
+		var result:Array<String> = [];
+		var rleRows:Array<String> = rlePattern.split("$");
+		for (row in rleRows) {
+			var expandedRow:String = "";
+			var number:String = "";
+			for (i in 0...row.length) {
+				var ch = row.charAt(i);
+				if (runCountMatcher.match(ch)) {
+					number += ch;
+				} else if (ch == "o") {
+					var runCount = 1;
+					if(number.length > 0) {
+						runCount = Std.parseInt(number);
+					}
+					for (i in 0...runCount) {
+						expandedRow += "o";
+						number = "";
+					}
+				} else if (ch == "b") {
+					var runCount = 1;
+					if(number.length > 0) {
+						runCount = Std.parseInt(number);
+					}
+					for (i in 0...runCount) {
+						expandedRow += "b";
+						number = "";
+					}
+				}
+			}
+			result.push(expandedRow);
+		}
+		
+		Sure.sure(result.length > 0);
+		trace(result);
+		return result;
 	}
 }
 
@@ -88,12 +185,36 @@ class Main {
 	private var camera:OrthographicCamera;
 	private var lifeEffect:GameOfLife;
 	private var gameDiv:DivElement;
+	
+	private var selectedPatternFileName(default, set):String; // Name of the currently selected pattern file (name of the corresponding member variable in the Patterns class)
+	
+	private var patternPresetListElement:SelectElement = cast Browser.document.getElementById("patternpresetlist"); // TODO
+	private var patternFileEditElement:TextAreaElement = cast Browser.document.getElementById("patternfileedit"); // TODO
 
 	private static function main():Void {
 		var main = new Main();
 	}
 
 	private inline function new() {
+		for (name in Type.getClassFields(Patterns)) {
+			var data = Reflect.field(Patterns, name);
+			
+			if (StringTools.endsWith(name, "rle")) {
+				//trace("its a rle"); // TODO
+				trace(name);
+				RLEReader.expandRle(data);
+			}
+			
+			var option = Browser.document.createOptionElement();
+			option.appendChild(Browser.document.createTextNode(name));
+			option.value = name;
+			patternPresetListElement.appendChild(option);
+		}
+		
+		patternPresetListElement.addEventListener("change", function() {
+			selectedPatternFileName = patternPresetListElement.value;
+		}, false);
+		
 		// Wait for the window to load before creating the sliders, starting the simulation input etc
 		Browser.window.onload = onWindowLoaded;
 	}
@@ -127,7 +248,6 @@ class Main {
 		// Setup WebGL renderer
         renderer = new WebGLRenderer( { antialias: true } );
 		renderer.autoClear = false;
-		renderer.setClearColor(new Color(0xff0000));
 		renderer.setPixelRatio(Browser.window.devicePixelRatio);
 		
 		// Scene setup
@@ -195,5 +315,20 @@ class Main {
 		var live:Bool = lifeEffect.isCellLive(x, y);
 		
 		trace(live);
+	}
+	
+	private function set_selectedPatternFileName(fileName:String):String {
+		selectedPatternFileName = fileName;
+		
+		var fileContent:Array<String> = Reflect.getProperty(Patterns, fileName);
+		Sure.sure(fileContent.length > 0);
+		
+		var content = "";
+		for (line in fileContent) {
+			content += line + "\r";
+		}
+		patternFileEditElement.value = content;
+		
+		return selectedPatternFileName;
 	}
 }
---
+++ PatternFileReaderMacro.hx
@@ -0,0 +1,58 @@
+package;
+
+import haxe.macro.Context;
+import haxe.macro.Expr.Access.APublic;
+import haxe.macro.Expr.Access.AStatic;
+import haxe.macro.Expr.Access.AInline;
+import haxe.macro.Expr.Field;
+import haxe.macro.Expr.FieldType.FVar;
+import util.FileReader;
+import sys.FileSystem;
+
+using StringTools;
+
+/**
+ * Build macro for reading Life patterns from files at compile time
+ */
+@:access(util.FileReader)
+class PatternFileReaderMacro {
+	public static function build(directoryPath:String):Array<Field> {
+		var fields = Context.getBuildFields();
+		
+		var splitter = new EReg("[\\r\\n]+", "g");
+		
+        try {
+            var files = FileSystem.readDirectory(directoryPath);
+            for (i in 0...files.length) {
+                var data = FileReader.loadFileAsString(directoryPath + "/" + files[i]);
+				
+				var file = files[i];
+				
+				// Take a filename e.g. 1beacon.cells and replace dots with underscores: 1beacon_cells
+				var name = file.replace(".", "_");
+				
+				var lines = splitter.split(data);
+				
+				// Remove empty lines
+				lines = Lambda.array(Lambda.filter(lines, function(line:String):Bool {
+					return line.length != 0 && line.trim().length != 0;
+				}));
+
+				var field = {
+					name: name,
+					doc: file,
+					meta: [],
+					access: [APublic, AStatic],
+					kind: FVar(macro:Array<String>, macro $v{lines}),
+					pos: Context.currentPos()
+				};
+				
+				fields.push(field);
+            }
+        } catch (e:Dynamic) {
+            Context.error('Failed to find directory $directoryPath: $e', Context.currentPos());
+        }
+		
+		return fields;
+	}
+}
