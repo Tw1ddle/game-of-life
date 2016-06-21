2166856ed4e936cc51f002b48cdabbe5c87360ee
sam <sam@silver42.karoo.co.uk>
2016-06-22 00:13:15 +0100

    Tweak to hopefully fix issue with white background on iOS

--- shaders/Life.hx
+++ shaders/Life.hx
@@ -8,8 +8,8 @@ class Life {
 	public static var uniforms = {
 		tUniverse: { type: "t", value: null },
 		texelSize: { type: "v2", value: new Vector2() },
-		liveColor: { type: "v4", value: new Vector4() },
-		deadColor: { type: "v4", value: new Vector4() }
+		liveColor: { type: "v4", value: new Vector4(1.0, 1.0, 1.0, 1.0) },
+		deadColor: { type: "v4", value: new Vector4(0.0, 0.0, 0.0, 1.0) }
 	};
 	public static var vertexShader = FileReader.readFile("shaders/life.vertex");
 	public static var fragmentShader = FileReader.readFile("shaders/life.fragment");
